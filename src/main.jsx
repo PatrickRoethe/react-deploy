a527be52960f593869e79185306f8659ac17b795
PatrickRoethe <146090971+PatrickRoethe@users.noreply.github.com>
2025-02-27 10:01:34 +0100

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { RouterProvider, createBrowserRouter } from "react-router-dom"; // <- Sjekk at dette er riktig
-
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
+
+// Lag en test-side
+function TestPage() {
+  return <h1>This is the Test Page</h1>;
+}
 
 // Definer ruter
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+  },
+  {
+    path: "/test", // 👈 Legger til Test Page her!
+    element: <TestPage />,
   },
 ]);
 
